511bbe74339a51f1704912a1145f236875262397
Natalya Iskanderova <natalya_iskanderova@epam.com>
2023-07-26 12:31:12 +0200

    refactor(components): chakra
    chakra

--- src/lib/pages/500/index.tsx
+++ src/lib/pages/500/index.tsx
@@ -1,22 +1,12 @@
 import { Typography, Grid } from '@mui/material'
 import { NextSeo } from 'next-seo';
 
-import MotionBox from '~/lib/components/motion/Box';
-
 const Page404 = () => {
 
   return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 3, md: 5 }} sx={{ padding: 4, marginTop: 4 }}>
       <Grid item xs={12} md={12} >
       <NextSeo title="404 Not Found" />
-      <MotionBox
-        animate={{ y: 20 }}
-        transition={{ repeat: Infinity, duration: 2, repeatType: 'reverse' }}
-        width={{ base: '100%', sm: '70%', md: '60%' }}
-        margin="0 auto"
-      >
-      </MotionBox>
-
         <Typography variant="h2" component="h2">
           Something went wrong
         </Typography>
